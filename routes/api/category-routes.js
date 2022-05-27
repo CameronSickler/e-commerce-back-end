a26d2bd7a564862e335b9e70952df737385a78e4
Cameron <sicklecj@gmail.com>
2022-05-27 17:06:26 -0500

    add category routes find one category code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,30 +4,70 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+
   // find all categories
   Category.findAll({
+
     include: [
       {
         model: Product,
         attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
       }
     ]
+
   })
     .then(dbCategoryData => res.json(dbCategoryData))
     .catch(err => {
+
       console.log(err);
       res.status(500).json(err);
+
     });
+
 });
 
 router.get('/:id', (req, res) => {
+
   // find one category by its `id` value
-  //Category.findOne
-  //grill req for id
-  //need model and attributes
-  //response
+  Category.findOne({
+
+    where: {
+
+      id: req.params.id
+
+    },
+
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+
+  })
+
+    .then(dbCategoryData => {
+
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'This id does not match a category' });
+        return;
+      }
+
+      res.json(dbCategoryData);
+
+    })
+
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+
+    });
+
   // be sure to include its associated Products
 });
+
+
+
 
 router.post('/', (req, res) => {
   // create a new category
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -66,12 +66,16 @@ router.get('/:id', (req, res) => {
         res.status(404).json({ message: 'This id does not match a product' });
         return;
       }
+
       res.json(dbProductData);
+
     })
+
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+
 });
 
 
@@ -92,71 +96,91 @@ router.post('/', (req, res) => {
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
+
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
+
           return {
             product_id: product.id,
             tag_id,
           };
         });
+
         return ProductTag.bulkCreate(productTagIdArr);
+
       }
+
       // if no product tags, just respond
       res.status(200).json(product);
+
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
     });
+
 });
-
-
 
 
 // update product
 router.put('/:id', (req, res) => {
-  // update product data
 
+  // update product data
   //Product.update
   Product.update(req.body, {
+
     where: {
       id: req.params.id,
     },
+
   })
     .then((product) => {
+
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
+
     })
     .then((productTags) => {
+
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
+
       // create filtered list of new tag_ids
       const newProductTags = req.body.tagIds
+
         .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
+
           return {
             product_id: req.params.id,
             tag_id,
           };
+
         });
+
       // figure out which ones to remove
       const productTagsToRemove = productTags
+
         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
         .map(({ id }) => id);
 
       // run both actions
       return Promise.all([
+
         ProductTag.destroy({ where: { id: productTagsToRemove } }),
         ProductTag.bulkCreate(newProductTags),
+
       ]);
+
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
+
       // console.log(err);
       res.status(400).json(err);
+
     });
 });
-
 
 
 
@@ -164,21 +188,29 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
 
   Product.destroy({
+
     where: {
       id: req.params.id
     }
+
   })
     .then(dbProductData => {
+
       if (!dbProductData) {
+
         res.status(404).json({ message: 'This id does not match a product' });
         return;
+
       }
+
       res.json(dbProductData);
+
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+
 });
 
 

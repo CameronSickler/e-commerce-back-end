eb59c7a1dd57afe0fa885fe783dbbb48e42fd311
Cameron <sicklecj@gmail.com>
2022-05-27 18:35:36 -0500

    uncomment categoryRoutes and tagRoutes for testing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const productRoutes = require('./product-routes');
-// const categoryRoutes = require('./category-routes');
-// const tagRoutes = require('./tag-routes');
+const categoryRoutes = require('./category-routes');
+const tagRoutes = require('./tag-routes');
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);

795bed588171cffd75679d9b90363e256cfbf8e3
Cameron <sicklecj@gmail.com>
2022-05-27 14:17:15 -0500

    add product routes delete code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -161,14 +161,27 @@ router.put('/:id', (req, res) => {
 
 
 
-
+// delete one product by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
 
-  //Product.destroy
-  //where grill for id in req params
-  // response
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
 
 
 

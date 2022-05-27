bdc711c408795df89c663f0906ffe1b68eff0284
Cameron <sicklecj@gmail.com>
2022-05-27 18:01:22 -0500

    add get request for single tag by id code to tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,6 +3,8 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+
+// find all tags
 router.get('/', (req, res) => {
 
   Tag.findAll({
@@ -25,31 +27,67 @@ router.get('/', (req, res) => {
 
 });
 
+
+
+// find a single tag by its `id`
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  //Tag.findOne
-  //grill for req id
-  //include attributes
-  //response
-  // be sure to include its associated Product data
+
+  Tag.findOne({
+
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      }
+    ]
+
+  })
+    .then(dbTagData => {
+
+      if (!dbTagData) {
+
+        res.status(404).json({ message: 'This id does not have a matching tag' });
+        return;
+
+      }
+
+      res.json(dbTagData);
+
+    })
+    .catch(err => {
+
+      console.log(err);
+      res.status(500).json(err);
+
+    });
+
 });
 
+
+
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
   //Tag.create
   //grill for name
   //response
 });
 
+
+
+// update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
   //Tag.update
   //grill req for id
   //response
 });
 
+
+
+// delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
   //Tag.destroy
   //grill req for id
   //response

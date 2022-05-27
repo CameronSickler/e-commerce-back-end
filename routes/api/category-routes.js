07efa4b16b29e993ff71a7ca6c16d45d8452abe3
Cameron <sicklecj@gmail.com>
2022-05-27 16:49:35 -0500

    add psuedo code to category routes to guide coding process

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,39 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  //Category.findAll
+  //need model and attributes
+  //response
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  //Category.findOne
+  //grill req for id
+  //need model and attributes
+  //response
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  //Category.create
+  //response
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  //Category.update
+  //grill req for id
+  //response
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  //Category.destroy
+  //grill req for id
+  //response
 });
 
 module.exports = router;

63efa353c85fed9cbc527ecd4e00ea5930666412
Cameron <sicklecj@gmail.com>
2022-05-27 14:18:57 -0500

    update 404 message to be consistent with get one product by id 404 message

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -171,7 +171,7 @@ router.delete('/:id', (req, res) => {
   })
     .then(dbProductData => {
       if (!dbProductData) {
-        res.status(404).json({ message: 'No product found with this id' });
+        res.status(404).json({ message: 'This id does not match a product' });
         return;
       }
       res.json(dbProductData);
